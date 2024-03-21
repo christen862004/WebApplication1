9bf375043a4c68a25eed3860d492f0ca12ac590f
Christen <christen.foad@gmail.com>
2024-03-21 13:35:15 +0200

    Add project files.

---
+++ WebApplication1/Hubs/ChatHub.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace WebApplication1.Hubs
+{
+    public class ChatHub:Hub
+    {
+        //Abdalla ==>Moudy Thorw Hub
+        public async Task SendMessage(string name,string message,int age)
+        {
+            //Context.UserIdentifier
+            //Context.ConnectionId
+            //logic db
+            //send MEssage To Another connected Client
+            await Clients.All.SendAsync("ReciveNewMessage",name,message);
+            
+
+        }
+        public override Task OnConnectedAsync()
+        {
+            
+            return base.OnConnectedAsync();
+        }
+    }
+    
+}
